549b2f0d83dfaa6f2c4051e6aac45e3ea54d5def
Anton_Shevchuk <ssniperok@gmail.com>
2019-01-21 12:05:47 +0200

    Chapter 7 finished

--- Chapter5/WebSearch.py
+++ Chapter5/WebSearch.py
@@ -1,15 +1,33 @@
+import mysql.connector
 import vsearch
-from flask import Flask, render_template, request, escape
+from flask import Flask, escape, render_template, request
 
 app = Flask(__name__)
 
 
 def log_request(req: 'flask_request', res: str) -> None:
-    with open('websearch.log', 'a') as logFile:
-        print(req.form, req.remote_addr, req.user_agent, res, file=logFile, sep='|')
+    dbconfig = {
+        "host": "127.0.0.1",
+        "user": "websearch",
+        "password": "search",
+        "database": "vsearchlogdb"
+    }
+
+    conn = mysql.connector.connect(**dbconfig)
+    cursor = conn.cursor()
+
+    _sqlInsert = """insert into logs
+    (phrase, letters, ip, user_agent, results)
+    values
+    (%s, %s, %s, %s, %s)"""
+
+    cursor.execute(_sqlInsert, (req.form['phrase'], req.form['letters'], req.remote_addr, req.user_agent.browser, res))
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 
-@app.route('/search4', methods=['POST'])  # indicates URL where this function will be invoked and methods that are allowed for this URL
+@app.route('/search4', methods = ['POST'])  # indicates URL where this function will be invoked and methods that are allowed for this URL
 def do_search() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
@@ -18,7 +36,7 @@ def do_search() -> 'html':
     result = str(vsearch.search4letters(phrase, letters))
     # renders template Result.html from the templates folder with provided arguments
     log_request(request, result)
-    return render_template('Results.html', the_title=title, the_phrase=phrase, the_letters=letters, the_results=result)
+    return render_template('Results.html', the_title = title, the_phrase = phrase, the_letters = letters, the_results = result)
 
 
 @app.route('/viewlog')
@@ -30,14 +48,14 @@ def view_log() -> 'html':
             content.append([])
             for item in line.split('|'):
                 content[-1].append(escape(item))
-    return render_template('ViewLog.html', the_title='Logging', the_row_titles=titles, the_data=content)
+    return render_template('ViewLog.html', the_title = 'Logging', the_row_titles = titles, the_data = content)
 
 
 @app.route('/')
 @app.route('/entry')
 def landing_page() -> 'html':
-    return render_template('Entry.html', the_title='WebsearchApp')
+    return render_template('Entry.html', the_title = 'WebsearchApp')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = True)
--- Chapter7/DbApi.py
+++ Chapter7/DbApi.py
@@ -10,6 +10,36 @@ dbconfig = {
 conn = mysql.connector.connect(**dbconfig)
 cursor = conn.cursor()
 
-_sql = """show tables"""
-cursor.execute(_sql)
+_sqlShowTables = """show tables"""
+cursor.execute(_sqlShowTables)
 print(cursor.fetchall())
+
+print("======================")
+
+_sqlDescribe = """describe logs"""
+cursor.execute(_sqlDescribe)
+result = cursor.fetchall()
+
+for row in result:
+    print(row)
+
+print("======================")
+
+_sqlInsert = """insert into logs
+(phrase, letters, ip, user_agent, results)
+values
+(%s, %s, %s, %s, %s)"""
+
+cursor.execute(_sqlInsert, ('hitch-hiker', 'aeiou', '127.0.0.1', 'Firefox', "{'e', 'i'}"))
+
+conn.commit()  # force write all inserts into DB
+
+_sqlSelect = """select * from logs"""
+cursor.execute(_sqlSelect)
+for row in cursor.fetchall():
+    print(row)
+
+print("======================")
+
+cursor.close()
+conn.close()
