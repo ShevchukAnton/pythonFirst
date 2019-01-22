d06c8551862e3917828f9d5ae664bd58b2828daf
Anton_Shevchuk <ssniperok@gmail.com>
2019-01-22 16:32:03 +0200

    Chapter 8 done; Started Chapter 9;

---
+++ Chapter8/Count.py
@@ -0,0 +1,7 @@
+import Chapter8.CountFromBy as counter
+
+res = counter.CountFromBy(step = 2)
+
+for i in range(10):
+    res.increment()
+    print(res)
--- Chapter8/CountFromBy.py
+++ Chapter8/CountFromBy.py
@@ -1,6 +1,12 @@
 class CountFromBy:
-    counter = 0
 
-    def increment(self, start: int = counter, step: int = 1) -> None:
-        start += step
+    def __init__(self, start: int = 0, step: int = 1) -> None:
+        """initialize object with provided or default data"""
         self.counter = start
+        self.step = step
+
+    def __repr__(self) -> str:
+        return str(self.counter)
+
+    def increment(self) -> None:
+        self.counter += self.step
