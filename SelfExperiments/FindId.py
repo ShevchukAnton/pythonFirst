0963329fd8ce95c3ddd4a6ebaee95b8802b32873
Anton_Shevchuk <ssniperok@gmail.com>
2019-10-21 23:12:34 +0300

    Added chrome driver for linux

--- SelfExperiments/FindId.py
+++ SelfExperiments/FindId.py
@@ -2,6 +2,7 @@
 # And download driver for browser that you are going to use (change path to that shit below)
 # Usage example - `python FindId.py https://www.acer.com/ac/en/GB/search D:\Python\Resources\ids`
 import os
+import platform
 import sys
 
 from selenium import webdriver
@@ -10,9 +11,16 @@ from selenium import webdriver
 def find_ids(url, file_with_ids):
     # parent dir for this project
     dirname = os.path.dirname(os.path.dirname(__file__))
-    driver_path = os.path.join(dirname, 'Resources/chromedriver.exe')
-    driver = webdriver.Chrome(driver_path)
+    osType = platform.system()
+    if 'windows' in osType.lower():
+        driver_path = os.path.join(dirname, 'Resources/chromedriver')
+    elif 'linux' in osType.lower():
+        driver_path = os.path.join(dirname, 'Resources/chromedriver.exe')
+    else:
+        print('Cant detect what kind of driver should be used. OS - ' + osType)
+        exit(1)
 
+    driver = webdriver.Chrome(driver_path)
     ids_file = os.path.join(dirname, 'Resources/Founded_ids.txt')
     output = open(ids_file, 'w')
 
